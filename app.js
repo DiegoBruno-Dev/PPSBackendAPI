cac0727472886444d88e01f257182ce2aa6a5e4f
DiegoXeneize <18diegobruno18@gmail.com>
2025-04-28 00:10:03 -0300

    More changes

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 const indexRoutes = require('./routes/index')
+
+app.use(cors())
 
 app.use(express.json())
 
